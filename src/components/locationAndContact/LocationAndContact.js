0a8013f82846cfea88607b60ed37c082d51fdee9
alishersodiqov09 <alishersodiqov09@gmail.com>
2023-09-13 15:10:46 +0500

    7 page

--- src/components/locationAndContact/LocationAndContact.js
+++ src/components/locationAndContact/LocationAndContact.js
@@ -2,46 +2,165 @@ import { Col, Form, FormGroup, FormText, Input, Label, Row } from "reactstrap";
 import "./style.css";
 import SideBar from "./sideBar/SideBar";
 import LocationNav from "./locationNav/LocationNav";
+import { useState } from "react";
+import { Icon } from '@iconify/react';
+import ContinueBtn from "./continueBtn/ContinueBtn";
 
 function LocationAndContact() {
+
+    const [plusNumbers, setPlusNumbers] = useState(false);
+    const [switches, setSwitches] = useState(false);
+
+    const isOpenNumber = () => setPlusNumbers(!plusNumbers);
+    const openSwitches = () => setSwitches(!switches);
+
     return (
         <div className="loc_main">
-            <Row className="">
-                <Col className="col-3">
+            <Row>
+                <Col className="col-2">
                     <SideBar />
                 </Col>
-                <Col className="col-9 loc_main-box">
+                <Col className="col-10 loc_main-box">
                     <LocationNav />
                     <Row>
-                        <Col md="6">
+                        <Col md="6" className="ps-5 pe-5">
                             <h5>Регистрация компании</h5>
                             <h3>Адреса и контакты</h3>
                             <Form>
-                                <FormGroup className="mt-5" floating>
+                                <FormGroup className="mt-4">
                                     <Input
+                                        className="loc_main-input"
                                         id="exampleAdres"
                                         placeholder="Адрес"
                                         type="text"
                                     />
-                                    <Label for="exampleAdres">
+                                    {/* <Label for="exampleAdres">
                                         Адрес
-                                    </Label>
-                                    <FormText className="ms-1">Укажите адрес на карте</FormText>
+                                    </Label> */}
+                                    <FormText className="loc_main-text">Укажите адрес на карте</FormText>
                                 </FormGroup>
-                                <FormGroup className="mt-4" floating>
+                                <FormGroup>
                                     <Input
+                                        className="loc_main-input"
                                         id="exampleAdresTwo"
                                         placeholder="Уточнения адреса"
                                         type="text"
                                     />
-                                    <Label for="exampleAdresTwo">
+                                    {/* <Label for="exampleAdresTwo">
                                         Уточнения адреса
-                                    </Label>
-                                    <FormText className="ms-1">Например: вход слева от красных ворот</FormText>
+                                    </Label> */}
+                                    <FormText className="loc_main-text">Например: вход слева от красных ворот</FormText>
                                 </FormGroup>
+                                <FormGroup>
+                                    <Input
+                                        className="loc_main-input"
+                                        id="exampleAdresKomp"
+                                        placeholder="Название филиала"
+                                        type="text"
+                                    />
+                                    {/* <Label for="exampleAdresKomp">
+                                        Название филиала
+                                    </Label> */}
+                                    <FormText className="loc_main-text">Выберете удобное для вас название, его увидят только менеджеры компании</FormText>
+                                </FormGroup>
+                                <FormGroup>
+                                    <Input
+                                        className="loc_main-input"
+                                        id="exampleNumber"
+                                        placeholder="Номер телефона"
+                                        type="number"
+                                    />
+                                    {/* <Label for="exampleNumber">
+                                        Номер телефона
+                                    </Label> */}
+                                    <FormText className="loc_main-text">Выберете удобное для вас название, его увидят только менеджеры компании</FormText>
+                                </FormGroup>
+
+                                {plusNumbers && plusNumbers
+                                    ? <FormGroup>
+                                        <Input
+                                            className="loc_main-input"
+                                            id="exampleNumberTwo"
+                                            placeholder="Добавьте номер телефона еще раз"
+                                            type="number"
+                                        />
+                                        {/* <Label for="exampleNumberTwo">
+                                            Добавьте номер телефона еще раз
+                                        </Label> */}
+                                        <FormText className="loc_main-text">Выберете удобное для вас название, его увидят только менеджеры компании</FormText>
+                                    </FormGroup>
+                                    : ""
+                                }
+                                <p onClick={isOpenNumber} className="loc_plus-par">Добавить Номер телефона</p>
                             </Form>
+
+                            <p className="loc_work">Режим работы</p>
+                            <FormGroup switch>
+                                <Input
+                                    className="py-2"
+                                    id="switches"
+                                    type="switch"
+                                    checked={switches}
+                                    onClick={() => {
+                                        setSwitches(!switches);
+                                        openSwitches();
+                                    }}
+                                />
+                                <Label check for="switches">Круглосуточно</Label>
+                            </FormGroup>
+
+                            {switches && switches
+                                ? <Row>
+                                    <Col className="loc_main-watches g-4 me-2 ms-2 text-center">
+                                        <p className="mt-2 mb-2 loc_main-watch">пн</p>
+                                        <p>09:00</p>
+                                        <p style={{ color: "#85858791" }}><Icon icon="tabler:point-filled" /></p>
+                                        <p className="mt-2 mb-2">18:00</p>
+                                    </Col>
+                                    <Col className="loc_main-watches g-4 me-2 text-center">
+                                        <p className="mt-2 mb-2 loc_main-watch">вт</p>
+                                        <p>09:00</p>
+                                        <p style={{ color: "#85858791" }}><Icon icon="tabler:point-filled" /></p>
+                                        <p className="mt-2 mb-2">18:00</p>
+                                    </Col>
+                                    <Col className="loc_main-watches g-4 me-2 text-center">
+                                        <p className="mt-2 mb-2 loc_main-watch">ср</p>
+                                        <p>09:00</p>
+                                        <p style={{ color: "#85858791" }}><Icon icon="tabler:point-filled" /></p>
+                                        <p className="mt-2 mb-2">18:00</p>
+                                    </Col>
+                                    <Col className="loc_main-watches g-4 me-2 text-center">
+                                        <p className="mt-2 mb-2 loc_main-watch">чт</p>
+                                        <p>09:00</p>
+                                        <p style={{ color: "#85858791" }}><Icon icon="tabler:point-filled" /></p>
+                                        <p className="mt-2 mb-2">18:00</p>
+                                    </Col>
+                                    <Col className="loc_main-watches g-4 me-2 text-center">
+                                        <p className="mt-2 mb-2 loc_main-watch">пт</p>
+                                        <p>09:00</p>
+                                        <p style={{ color: "#85858791" }}><Icon icon="tabler:point-filled" /></p>
+                                        <p className="mt-2 mb-2">18:00</p>
+                                    </Col>
+                                    <Col className="loc_main-watches g-4 me-2 text-center">
+                                        <p className="mt-2 mb-2 loc_main-watch">сб</p>
+                                        <p>09:00</p>
+                                        <p style={{ color: "#85858791" }}><Icon icon="tabler:point-filled" /></p>
+                                        <p className="mt-2 mb-2">18:00</p>
+                                    </Col>
+                                    <Col className="loc_main-watches g-4 me-2 text-center">
+                                        <p className="mt-2 mb-2 loc_main-watch">вс</p>
+                                        <p style={{ color: "#85858791", marginTop: "1.9rem" }}><Icon icon="tabler:sun" /></p>
+                                    </Col>
+                                </Row>
+                                : ""
+                            }
+
+                            <ContinueBtn />
                         </Col>
-                        <Col md="6">dsgfd</Col>
+                        <Col md="6" className="mt-4">
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m24!1m12!1m3!1d95901.41990287435!2d69.2518912!3d41.3106176!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m9!3e6!4m1!2z0YPQuy4g0JTQttCw0LzQuCwgNSAo0LrRgNGD0LMg0JTQttCw0LzQuCkgMTAwMDU3INCi0LDRiNC60LXQvdGCIFVa!4m5!1s0x38ae8b0cc379e9c3%3A0xa5a9323b4aa5cb98!2z0KLQvnNoa2VudA!3m2!1d41.299495799999995!2d69.2400734!5e0!3m2!1suz!2s!4v1694541826960!5m2!1suz!2s"
+                                style={{ width: "93%", height: "87vh", border: "none" }} loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                        </Col>
                     </Row>
                 </Col>
             </Row>
--- src/components/locationAndContact/sideBar/SideBar.js
+++ src/components/locationAndContact/sideBar/SideBar.js
@@ -1,31 +1,29 @@
 import "./styleSideBar.css";
 import imgLogo from "../img/logo.png";
+import { Icon } from "@iconify/react";
 
 function SideBar() {
     return (
         <>
-            <img src={imgLogo} alt="img" className="w-50 ms-4 mt-4" />
-            <p className="loc_contact mt-5">
+            <img src={imgLogo} alt="img" className="mt-4 mb-4"
+                style={{ cursor: "pointer", width: "70%", height: "5rem" }} />
+            <p className="loc_contact">
                 <span>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="20" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16">
-                        <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425a.247.247 0 0 1 .02-.022Z" />
-                    </svg>
+                    <Icon icon="ri:check-line" />
                 </span>
                 Язык и часовой пояс
             </p>
-            <p className="loc_contact mt-5">
+            <p className="loc_contact">
                 <span>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="20" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16">
-                        <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425a.247.247 0 0 1 .02-.022Z" />
-                    </svg>
+                    <Icon icon="ri:check-line" />
                 </span>
                 Информация о компании
             </p>
-            <p className="loc_contact loc_contactNum mt-5">
+            <p className="loc_contact loc_contactNum">
                 <span>3</span>
                 Адреса и контакты
             </p>
-            <p className="loc_contact loc_contactNum mt-5">
+            <p className="loc_contact loc_contactNum">
                 <span>4</span>
                 Бонусная программа
             </p>
